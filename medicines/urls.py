7f0b9ddc323d04ae121f4e97be35ddf2dd20681a
sravan-kumar-ta <sravan0095@gmail.com>
2021-12-24 22:23:18 +0530

    make some changes and finilized medicine app

--- medicines/urls.py
+++ medicines/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('medicine/', views.medicine, name='manage-medicine'),
     path('add_medicine/', views.add_medicine, name='add-medicine'),
     path('update_medicine/<int:m_id>/', views.update_medicine, name='update-medicine'),
+    path('delete_medicine/<int:m_id>/', views.delete_medicine, name='delete-medicine'),
 ]
--- medicines/views.py
+++ medicines/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.text import slugify
 
@@ -12,14 +11,17 @@ def home(request):
 
 def medicine(request):
     medicines = Medicine.objects.all().order_by('-id')
-    return render(request, 'manage_medicine.html', {'medicines': medicines})
+    return render(request, 'medicine/manage_medicine.html', {'medicines': medicines})
 
 
 def add_medicine(request):
     if request.method == 'POST':
         name = request.POST.get('m_name')
         category_id = request.POST.get('category')
-        image = request.FILES['medicine_image']
+        if request.FILES:
+            image = request.FILES['medicine_image']
+        else:
+            image = None
         manufacture = request.POST.get('manufacture')
         price = request.POST.get('price')
         is_active = request.POST.get('active')
@@ -32,23 +34,27 @@ def add_medicine(request):
             is_active = 'False'
         medicine = Medicine(title=name, slug=slug, category=category, image=image, manufactured_by=manufacture, price=price, is_active=is_active)
         medicine.save()
-        # return HttpResponseRedirect('/')
-        return render(request, 'add_medicine.html')
+
+        return redirect('manage-medicine')
     else:
         categ = Category.objects.all()
-        return render(request, 'add_medicine.html', {'categories': categ})
+        return render(request, 'medicine/add_medicine.html', {'categories': categ})
 
 
 def update_medicine(request, m_id):
     if request.method != 'POST':
         item = get_object_or_404(Medicine, id=m_id)
         category = Category.objects.all()
-        return render(request, 'add_medicine.html', {'medicine': item, 'categories': category})
+        return render(request, 'medicine/add_medicine.html', {'medicine': item, 'categories': category})
     else:
         item = get_object_or_404(Medicine, id=m_id)
         name = request.POST.get('m_name')
         category_id = request.POST.get('category')
-        # image = request.FILES['medicine_image']
+        if request.FILES:
+            image = request.FILES['medicine_image']
+            item.image.delete()
+        else:
+            image = item.image
         manufacture = request.POST.get('manufacture')
         price = request.POST.get('price')
         is_active = request.POST.get('active')
@@ -63,9 +69,17 @@ def update_medicine(request, m_id):
         item.title = name
         item.slug = slug
         item.category = category
+        item.image = image
         item.manufactured_by = manufacture
         item.price = price
         item.is_active = is_active
         item.save()
 
         return redirect('manage-medicine')
+
+
+def delete_medicine(request, m_id):
+    item = get_object_or_404(Medicine, id=m_id)
+    item.image.delete()
+    item.delete()
+    return redirect('manage-medicine')
