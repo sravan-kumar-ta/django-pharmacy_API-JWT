59bbdf6a4aa7d0b3b0788188eec76d58162d08bc
sravan-kumar-ta <sravan0095@gmail.com>
2022-01-18 00:11:36 +0530

    only can purchase in-stock items, and decrese items when buy medicines

--- medicine_API/views.py
+++ medicine_API/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListCreateAPIView, GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from medicines.models import Medicine
 from .models import Order
@@ -25,6 +26,20 @@ class OrderView(GenericAPIView):
     def post(self, request):
         serializer = OrderSerializer(data=request.data, many=True)
         if serializer.is_valid():
+            for item in request.data:
+                medicine_id = item["medicine"]
+                quantity = item["quantity"]
+                medicine = get_object_or_404(Medicine, id=medicine_id)
+                if medicine.stock < quantity:
+                    return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
+
+            for item in request.data:
+                medicine_id = item["medicine"]
+                quantity = item["quantity"]
+                medicine = get_object_or_404(Medicine, id=medicine_id)
+                medicine.stock -= quantity
+                medicine.save()
+
             serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
